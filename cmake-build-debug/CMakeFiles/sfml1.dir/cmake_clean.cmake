73bccce191bb51585538a4f299af94b293472fc0
Aur3ll <102746516+Aur3ll@users.noreply.github.com>
2022-05-21 22:10:57 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/sfml1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml1.dir/Balle.cpp.o"
+  "CMakeFiles/sfml1.dir/Brique.cpp.o"
+  "CMakeFiles/sfml1.dir/Hud.cpp.o"
+  "CMakeFiles/sfml1.dir/Raquette.cpp.o"
+  "CMakeFiles/sfml1.dir/Sound.cpp.o"
+  "CMakeFiles/sfml1.dir/Special.cpp.o"
+  "CMakeFiles/sfml1.dir/main.cpp.o"
+  "sfml1"
+  "sfml1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
